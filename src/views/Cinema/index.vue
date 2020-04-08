15f394c1211c2e00a955b80f773d40b8f02becb6
littleyayap <1092254988@qq.com>
2020-04-09 00:37:28 +0800

    配置路由

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+  <div id="main">
+    <Header title="喵喵影院"/>
+    <Tabbar />
+  </div>
+</template>
+ 
+<script>
+
+import Header from '@/components/Header';
+import Tabbar from '@/components/Tabbar';
+
+export default {
+  name: "cinema",
+  data() {
+    return {};
+  },
+
+  components: {
+    Header,
+    Tabbar
+  }
+};
+</script>
+<style lang='scss' scoped>
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+  <div id="main">
+    <Header title="我的影院"/>
+    <Tabbar />
+  </div>
+</template>
+ 
+<script>
+
+import Header from '@/components/Header';
+import Tabbar from '@/components/Tabbar';
+
+export default {
+  name: "mine",
+  data() {
+    return {};
+  },
+
+  components: {
+    Header,
+    Tabbar
+  }
+};
+</script>
+<style lang='scss' scoped>
+</style>
